1dfe81c217abc23ae6bf47deaec449ac88a48374
jeanfson <124371819+jeanfson@users.noreply.github.com>
2023-08-17 15:42:09 -0300

    upload site EmpServicesBr

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./lib/bootstrap-4.6.2-dist/css/bootstrap.min.css">
+
+    <title>EmpServices</title>
+  </head>
+  <body>
+    <?php include "navbar.html" ?>
+
+    <br /><br /><br /><br />
+
+    <div class="">
+        <?php include "paginas.php" ?>
+    </div>
+
+    <div>
+        <?php include "footer.html" ?>
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="./lib/jquery-3.5.1.slim.min.js"></script>
+    <script src="./lib/bootstrap-4.6.2-dist/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="./lib/jquery-3.5.1.slim.min.js"></script>
+    <script src="./lib/popper-1.16.1.min.js"></script>
+    <script src="./lib/bootstrap-4.6.2-dist/js/bootstrap.min.js"></script>
+    -->
+  </body>
+</html>
